aabeb5e3d0798e6901bdf29776aacb371e53ee6f
Sharmae09 <sharmae.code@gmail.com>
2025-02-11 23:29:19 +0800

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Nuxt.app',
+  appName: 'Nuxt',
+  webDir: '.output/public',
+  
+};
+
+export default config;
